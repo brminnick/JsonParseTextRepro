030ba462a67d538cb31461c732f8c891798efce2
Brandon Minnick <brminnick@users.noreply.github.com>
2019-09-12 19:01:26 -0700

    Removed Refit

--- JsonParseTextRepro/ApiService.cs
+++ JsonParseTextRepro/ApiService.cs
@@ -2,32 +2,29 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Refit;
 
 namespace JsonParseTextRepro
 {
     public class ApiService
     {
-        const string _baseApiUrl = "https://pundaycdn.azureedge.net";
-        readonly IApiService _apiServiceClient = RestService.For<IApiService>(CreateHttpClient(_baseApiUrl));
+        readonly HttpClient _client = new HttpClient
+        {
+            BaseAddress = new Uri("https://pundaycdn.azureedge.net"),
+            DefaultRequestHeaders =
+            {
+                { "ZUMO-API-VERSION", "2.0.0" },
+                { "Accept-Encoding", "gzip" },
+                { "Accept", "application/json" }
+            }
+        };
 
-        public Task<PunModel> GetPun(int punNumber) => _apiServiceClient.GetAzurePunModel(punNumber);
         public async Task<PunModel> GetPun()
         {
-            using (var client = new HttpClient())
+            using (var response = await _client.GetAsync("/tables/AzurePunModel/200").ConfigureAwait(false))
             {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Add("ZUMO-API-VERSION", "2.0.0");
-                client.DefaultRequestHeaders.AcceptEncoding.Add(new System.Net.Http.Headers.StringWithQualityHeaderValue("gzip"));
-
-                using (var response = await client.GetAsync($"{_baseApiUrl}/tables/AzurePunModel/200").ConfigureAwait(false))
-                {
-                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    return JsonConvert.DeserializeObject<PunModel>(json);
-                }
+                var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return JsonConvert.DeserializeObject<PunModel>(json);
             }
         }
-
-        static HttpClient CreateHttpClient(string url) => new HttpClient { BaseAddress = new Uri(url) };
     }
 }
