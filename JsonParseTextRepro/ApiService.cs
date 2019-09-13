5b918073ae4313cf60352a121b5f050323d78544
Brandon Minnick <brminnick@users.noreply.github.com>
2019-09-12 19:49:33 -0700

    Removed BaseAddress

--- JsonParseTextRepro/ApiService.cs
+++ JsonParseTextRepro/ApiService.cs
@@ -9,7 +9,6 @@ namespace JsonParseTextRepro
     {
         readonly static HttpClient _client = new HttpClient
         {
-            BaseAddress = new Uri("https://pundaycdn.azureedge.net"),
             DefaultRequestHeaders =
             {
                 { "ZUMO-API-VERSION", "2.0.0" },
@@ -20,7 +19,7 @@ namespace JsonParseTextRepro
 
         public static async Task<PunModel> GetPun()
         {
-            using (var response = await _client.GetAsync("/tables/AzurePunModel/200").ConfigureAwait(false))
+            using (var response = await _client.GetAsync("https://pundaycdn.azureedge.net/tables/AzurePunModel/200").ConfigureAwait(false))
             {
                 var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 return JsonConvert.DeserializeObject<PunModel>(json);
